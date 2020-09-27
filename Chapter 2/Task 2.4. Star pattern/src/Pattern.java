c4adeb8708630ddf079ef035ba2c641f8936b564
pawel <pawel@pawel>
2020-09-27 22:08:52 +0200

    Task 2.4. Done

---
+++ Chapter 2/Task 2.4. Star pattern/src/Pattern.java
@@ -0,0 +1,31 @@
+/* Programming task assigned in T. Gaddis, Starting Out with Java, 7th ed.
+ * 
+ * Task 2.4. Star pattern
+ * 
+ * Instruction:
+ * 
+ * "Write a program that displays the following pattern:  
+ *  		
+ *		   *  
+ * 		  *** 
+ * 		 *****
+ * 		*******
+ * 		 *****  
+ * 		  *** 
+ * 		   *"
+ */
+
+public class Pattern {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("\t   *");
+		System.out.println("\t  ***");
+		System.out.println("\t *****");
+		System.out.println("\t*******");
+		System.out.println("\t *****");
+		System.out.println("\t  ***");
+		System.out.println("\t   *");
+		
+	}
+}
