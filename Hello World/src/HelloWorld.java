085dcfeaecabe43e659e567995d524ed4c013e48
jack <jack@paul-X540YA>
2020-09-27 11:54:52 +0200

    Hello world and hello GitHub : )

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,11 @@
+// Classic way to kick-off any programming journey
+// and here also a hello to GitHub
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Hello world and hello GitHub! :)");
+		
+	}
+}
