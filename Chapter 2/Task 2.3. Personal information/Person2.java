3785f321462624fefdb1da425b7b4a0ec5226d21
pawel <pawel@pawel>
2020-09-27 17:42:08 +0200

    Chapter 2: Task 2.1, 2.2, 2.3, 2.4

---
+++ Chapter 2/Task 2.3. Personal information/Person2.java
@@ -0,0 +1,34 @@
+/* Programming task assigned in T. Gaddis, Starting Out with Java, 7th ed.
+ * 
+ * Task 2.3. Personal information
+ * 
+ * Instruction:
+ * 
+ * "Write a program that displays the following information, each on a separate line:
+ * -  Your name
+ * -  Your address, with city, state, and ZIP
+ * -  Your telephone number
+ * -  Your college major 
+ * 
+ * Although these items should be displayed on separate output lines, 
+ * use only a single println statement in your program."
+ * 
+ */
+
+public class Person2 {
+	
+	public static void main(String[] args) {
+		
+		String name = "John Smith";
+		
+		String fullAddress = "Yorkshire Street 35, New York, NY, 10001";
+		
+		String phoneNumber = "123456789";
+		
+		String universityMajor = "Computer Science";
+		
+		System.out.println("First and last name: " + name + "\nAddress: " + fullAddress + "\nTelephone number: " + phoneNumber + "\nUniversity Major: " + universityMajor);
+		
+	}
+
+}
