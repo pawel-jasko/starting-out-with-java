e5fcd69f93b9f3b70900b861da9dc940012460b6
pawel-jasko <71939378+pawel-jasko@users.noreply.github.com>
2020-09-28 17:15:49 +0200

    Update SalesPrediction.java

--- Chapter 2/Task 2.5. Sales Prediction/src/SalesPrediction.java
+++ Chapter 2/Task 2.5. Sales Prediction/src/SalesPrediction.java
@@ -5,9 +5,9 @@
  * Instruction:
  * 
  * "The East Coast sales division of a company generates 62 percent of total sales. Based on
-that percentage, write a program that will predict how much the East Coast division will
-generate if the company has $4.6 million in sales this year. Hint: Use the value 0.62 to
-represent 62 percent."
+ * that percentage, write a program that will predict how much the East Coast division will
+ * generate if the company has $4.6 million in sales this year. Hint: Use the value 0.62 to
+ * represent 62 percent."
  * 
  */
 
